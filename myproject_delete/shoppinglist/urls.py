4551772e80d5a96aa527394d4af88bdb6e424a1c
t22cs063yokoyama <t22cs063@gmail.com>
2024-12-06 11:38:17 +0900

    削除機能完成

--- myproject_delete/shoppinglist/urls.py
+++ myproject_delete/shoppinglist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ItemList, ItemDeleteView
+from .views import ItemList, ItemDeleteView, ItemAddView
 
 app_name='shoppinglist'
 urlpatterns = [
     path('list/', ItemList.as_view(),name='list'),
     path('delete', ItemDeleteView.as_view(), name='delete'),
+    path("delete/id=<int:pk>",ItemDeleteView.as_view(), name='delete'), 
+    path('add', ItemAddView.as_view(), name='add'),
 ]
--- myproject_delete/shoppinglist/views.py
+++ myproject_delete/shoppinglist/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Item
+from .models import Item, Shop
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView
 from .forms import ItemBuy, ItemIdForm, ItemForm
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 # Create your views here.
 
@@ -27,17 +28,22 @@ class ItemList(ListView):
         context['form'] = ItemBuy()
         return context
 
-class ItemDeleteView(TemplateView):
-    model = Item
-    template_name = 'shoppinglist/item_delete.html'
+class ItemDeleteView(UpdateView):
+   model = Item
+   template_name = 'shoppinglist/item_delete.html'
+   success_url = 'list/'
+   fields=[]
 
-    def post(self, request, *args, **kwargs):
-        item_id = self.request.POST.get('item_id')
+   def post(self, request, *args, **kwargs):
+        item_id = self.kwargs.get('pk')
+        item = self.request.POST.get('item_id')
         item = get_object_or_404(Item, pk=item_id)
         item.delete()
+        
         return HttpResponseRedirect(reverse('shoppinglist:list'))
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = ItemIdForm()
-        return context
+    
+class ItemAddView(CreateView):
+    model = Item
+    fields = ('name', 'item_url', 'count', 'buy_date', 'shop')
+    template_name = 'shoppinglist/item_add.html'
+    success_url = 'list/'
